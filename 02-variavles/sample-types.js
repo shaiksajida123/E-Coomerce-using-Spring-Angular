253163f25297947d186acb2f55518cec63cbe936
shaiksajida123 <66249630+shaiksajida123@users.noreply.github.com>
2021-12-15 12:13:03 +0530

    Add files via upload

---
+++ 02-variavles/sample-types.js
@@ -0,0 +1,13 @@
+var foubd = true;
+var grade = 88.6;
+var firstname = "sajida";
+var lastName = 'Kumar';
+// lets break it
+// foubd = 0;
+// grade = "A";
+// firstName = false;
+console.log(foubd);
+console.log(grade);
+console.log("Hi" + firstname + lastName);
+//use template strings 
+console.log('Hi $ {firstname} ${lastname}');
---
+++ 02-variavles/sample-types.ts
@@ -0,0 +1,20 @@
+let foubd: boolean=true;
+let grade: number=88.6;
+let firstname : string="sajida";
+let lastName: string = 'Kumar';
+
+
+// lets break it
+// foubd = 0;
+// grade = "A";
+// firstName = false;
+
+
+
+
+
+console.log(foubd);
+console.log(grade);
+console.log("Hi"+ firstname + lastName);
+//use template strings 
+console.log('Hi $ {firstname} ${lastname}');
