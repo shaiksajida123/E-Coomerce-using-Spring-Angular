11ef00f569bce3aec42ddc23203b116d0b8e0379
shaiksajida123 <66249630+shaiksajida123@users.noreply.github.com>
2021-12-15 12:49:19 +0530

    Add files via upload

---
+++ 03-loops-arrays/sprots.js
@@ -0,0 +1,12 @@
+var sprotOne = ["golf", "crirkcet", "tennis", "swiming", "sjaida"];
+// for(let i=0;i<sprotOne.length;i++){
+//     console.log(sprotOne[i]);
+// }
+//let use the simpliedfore
+for (var _i = 0, sprotOne_1 = sprotOne; _i < sprotOne_1.length; _i++) {
+    var tempsprots = sprotOne_1[_i];
+    if (tempsprots == "crirkcet") {
+        console.log(tempsprots + "<<my favaioute sprots");
+    }
+    console.log(tempsprots);
+}
---
+++ 03-loops-arrays/sprots.ts
@@ -0,0 +1,14 @@
+let sprotOne:string[]=["golf","crirkcet","tennis","swiming","sjaida"];
+
+// for(let i=0;i<sprotOne.length;i++){
+//     console.log(sprotOne[i]);
+// }
+
+//let use the simpliedfore
+
+for( let tempsprots of sprotOne){
+    if(tempsprots =="crirkcet"){
+        console.log(tempsprots + "<<my favaioute sprots");
+    }
+    console.log(tempsprots);
+}
