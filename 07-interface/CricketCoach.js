c818503b6489870efd65a49cf53754dcae942539
shaiksajida123 <66249630+shaiksajida123@users.noreply.github.com>
2021-12-22 14:32:16 +0530

    Add files via upload

---
+++ 07-interface/CricketCoach.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.CricketCoach = void 0;
+var CricketCoach = /** @class */ (function () {
+    function CricketCoach() {
+    }
+    CricketCoach.prototype.getDailyWorkout = function () {
+        return "Practice your spin bowling technique.";
+    };
+    return CricketCoach;
+}());
+exports.CricketCoach = CricketCoach;
---
+++ 07-interface/GolfCoach.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.GolfCoach = void 0;
+var GolfCoach = /** @class */ (function () {
+    function GolfCoach() {
+    }
+    GolfCoach.prototype.getDailyWorkout = function () {
+        return "Hit 100 balls at the golf range.";
+    };
+    return GolfCoach;
+}());
+exports.GolfCoach = GolfCoach;
